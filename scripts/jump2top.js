11cd97e1d930ad0fe61b2e656845c2c5786d1743
bijbrengen <slagter@bijbrengen.nl>
2018-09-22 22:09:11 +0200

    ID set to jump2top

--- scripts/jump2top.js
+++ scripts/jump2top.js
@@ -1,4 +1,4 @@
-jQuery("#backtotop").click(function ()
+jQuery("#jump2top").click(function ()
 {
     jQuery("body,html").animate(
     {
@@ -8,9 +8,9 @@ jQuery("#backtotop").click(function ()
 jQuery(window).scroll(function ()
 {
     if (jQuery(window).scrollTop() > 150) {
-        jQuery("#backtotop").addClass("visible");
+        jQuery("#jump2top").addClass("visible");
     } else {
-        jQuery("#backtotop").removeClass("visible");
+        jQuery("#jump2top").removeClass("visible");
     }
 });
 $('<form action="#"><select /></form>').appendTo("#mainav");$("<option />",{selected:"selected",value:"",text:"MENU"}).appendTo("#mainav select");$("#mainav a").each(function(){var e=$(this);if($(e).parents("ul ul ul").length>=1){$("<option />",{value:e.attr("href"),text:"- - - "+e.text()}).appendTo("#mainav select")}else if($(e).parents("ul ul").length>=1){$("<option />",{value:e.attr("href"),text:"- - "+e.text()}).appendTo("#mainav select")}else if($(e).parents("ul").length>=1){$("<option />",{value:e.attr("href"),text:""+e.text()}).appendTo("#mainav select")}else{$("<option />",{value:e.attr("href"),text:e.text()}).appendTo("#mainav select")}});$("#mainav select").change(function(){if($(this).find("option:selected").val()!=="#"){window.location=$(this).find("option:selected").val()}})
