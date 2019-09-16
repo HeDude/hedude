759fff5bde9f4764fae368e77327af0186be638c
bijbrengen <slagter@bijbrengen.nl>
2019-09-16 06:20:56 +0200

    Removed passing uri. API will determine itself

--- api/index.php
+++ api/index.php
@@ -5,8 +5,8 @@
 /*
 * \author      Rob Slagter
 * \copyright   2018 Rob Slagter, HeDude
-* \date        25-08-2018
-* \version     1.0.0
+* \date        16-09-2019
+* \version     1.0.1
 */
 //! The API handling class from the education server
 if ( empty( getenv ( 'API_PROGRAM_PATH' ) ) )
@@ -16,6 +16,7 @@ require_once getenv ( 'API_PROGRAM_PATH' );
 //! Open API en link to local information
 $api = new API( __DIR__ );
 
-//! Return API respons based on the calling URL
-$api->respond( $_SERVER[ 'REQUEST_URI' ] );
+//! Return API respons based on the calling URL which respond will determine by
+//! the server globals.
+$api->respond();
 exit;
